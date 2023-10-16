ae0fb0100bcfd22ffb7b94187532631f098d8d18
yaaman18 <gucchiyama8551@hotmail.co.jp>
2023-10-16 13:02:48 +0900

    44

--- src/App.svelte
+++ src/App.svelte
@@ -7,66 +7,74 @@
   let passwordWarning = "";
   let showPassword = false;
   let seedWarning = "";
+  let seedWordCount = 0;
+
+   $: seedWordCount = inputSeed.split(' ').filter(word => word.length > 0).length;
 
   function togglePasswordVisibility() {
     showPassword = !showPassword;
   }
 
   async function handleFormSubmit() {
-  seedWarning = "";
-  cipher = "";
-  try {
-    const wordlist_en:string = await invoke('read_wordlist_file');
-    const wordSet = new Set(wordlist_en.split('\n'));
-    const inputWords = inputSeed.split(' ');
+    seedWarning = "";
+    cipher = "";
+    try {
+      const wordlist_en: string = await invoke("read_wordlist_file");
+      const wordSet = new Set(wordlist_en.split("\n"));
+      const inputWords = inputSeed.split(" ");
 
-  if (inputWords.length < 12 || inputWords.length > 16) {
-  seedWarning = "シードフレーズは12単語から16単語までで入力してください";
-  return;
-  }
+      seedWordCount = inputWords.length;
 
-  const uniqueWordsSet = new Set(inputWords);
-    if (uniqueWordsSet.size !== inputWords.length) {
-      seedWarning = "シードフレーズに同じ単語を2回入力しないでください";
-      return;
-    }
-
-
-    for (let word of inputWords) {
-      if (!wordSet.has(word)) {
-        seedWarning = "シードフレーズの単語が間違っています";
+      if (inputWords.length < 12 || inputWords.length > 16) {
+        seedWarning = "シードフレーズは12単語から16単語までで入力してください";
         return;
       }
-    }
-  } catch (error) {
-    console.error("Error:", error);
-  }
 
+      const uniqueWordsSet = new Set(inputWords);
+      if (uniqueWordsSet.size !== inputWords.length) {
+        seedWarning = "シードフレーズに同じ単語を2回入力しないでください";
+        return;
+      }
+
+      for (let word of inputWords) {
+        if (!wordSet.has(word)) {
+          seedWarning = "シードフレーズの単語が間違っています";
+          return;
+        }
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
 
     try {
-        cipher = await invoke('wrap_handle_data', { inputSeed: inputSeed, password: password });
+      cipher = await invoke("wrap_handle_data", {
+        inputSeed: inputSeed,
+        password: password,
+      });
     } catch (error) {
-        console.error("Error invoking wrap_handle_data:", error);
+      console.error("Error invoking wrap_handle_data:", error);
     }
-}
+  }
 
-function copyToClipboard(text: string) {
-  navigator.clipboard.writeText(text).then(
-    function () {
-      console.log("コピー成功");
-    },
-    function (err) {
-      console.error("コピー失敗", err);
-    }
-  );
-}
+  function copyToClipboard(text: string) {
+    navigator.clipboard.writeText(text).then(
+      function () {
+        console.log("コピー成功");
+      },
+      function (err) {
+        console.error("コピー失敗", err);
+      },
+    );
+  }
 </script>
-
 
 <main>
   <h1>Nil Wallet: あなたのデジタル資産を安全に管理</h1>
   <h2>シンプルで安全なウォレット、手の中に。</h2>
-  <p>Nil Walletは、デジタル通貨の管理をよりシンプルで安全にするための革新的なウォレットアプリケーションです。ユーザーフレンドリーなインターフェイスと先進的なセキュリティ機能を組み合わせて、初心者から上級者まで、誰でも簡単にデジタル資産を管理できます。</p>
+  <p>
+    Nil
+    Walletは、デジタル通貨の管理をよりシンプルで安全にするための革新的なウォレットアプリケーションです。ユーザーフレンドリーなインターフェイスと先進的なセキュリティ機能を組み合わせて、初心者から上級者まで、誰でも簡単にデジタル資産を管理できます。
+  </p>
   <h3>主な機能:</h3>
   <ul>
     <li>セキュアな暗号化</li>
@@ -74,42 +82,78 @@ function copyToClipboard(text: string) {
     <li>直感的な操作</li>
     <li>カスタマーサポート</li>
   </ul>
-  <p>Nil Walletで、デジタル通貨の新しい時代を体験しましょう。今すぐダウンロードして、安全で便利なデジタル資産管理の世界へ飛び込みましょう。</p>
+  <p>
+    Nil
+    Walletで、デジタル通貨の新しい時代を体験しましょう。今すぐダウンロードして、安全で便利なデジタル資産管理の世界へ飛び込みましょう。
+  </p>
   <h3>暗号生成</h3>
-  <p>こちらにお手持ちのwalletのシードフレーズ（１２単語から１６単語まで）と復号する時に必要なパスワード（最低８文字）を入力してください。</p>
-  <p>パスワードは暗号文からシードフレーズを復元する時に必要なので絶対に忘れないでください。</p>
-  <p>入力するパスワードは半角英数字のみでスペースを入れずに入力してください。</p>
+  <p>
+    こちらにお手持ちのwalletのシードフレーズ（１２単語から１６単語まで）と復号する時に必要なパスワード（最低８文字）を入力してください。
+  </p>
+  <p>
+    パスワードは暗号文からシードフレーズを復元する時に必要なので絶対に忘れないでください。
+  </p>
+  <p>
+    入力するパスワードは半角英数字のみでスペースを入れずに入力してください。
+  </p>
   <p>尚オフラン環境でのみ利用可能になっています。</p>
-  <p>また、パソコンの中にシードフレーズもパスワードも保存しないので安心してご利用いただけます</p>
+  <p>
+    また、パソコンの中にシードフレーズもパスワードも保存しないので安心してご利用いただけます
+  </p>
   <form on:submit|preventDefault={handleFormSubmit}>
-  <label>
-    シードフレーズ:
+    <label>
+      シードフレーズ:
       <input type="text" bind:value={inputSeed} style="width: 100%;" />
-  </label>
-  {#if seedWarning}
-  <p style="color: red;">{seedWarning}</p>
-{/if}
+    </label>
+    <p>入力した単語数: {seedWordCount}</p>
+    {#if seedWarning}
+      <p style="color: red;">{seedWarning}</p>
+    {/if}
 
-  <label>
-  パスワード:
-  {#if showPassword}
-    <input type="text" bind:value={password} style="width: 100%;" minlength="4" maxlength="80" class="password-input" />
-    <button type="button" on:click={() => copyToClipboard(password)}>パスワードをコピー</button>
-  {:else}
-    <input type="password" bind:value={password} style="width: 100%;" minlength="8" class="password-input" />
-    <button type="button" on:click={() => copyToClipboard(password)}>パスワードをコピー</button>
-  {/if}
-  <button type="button" on:click={togglePasswordVisibility} class="toggle-button">{showPassword ? '非表示にする' : 'パスワードを表示する'}</button>
-  </label>
-  {#if passwordWarning}
-    <p style="color: red;">{passwordWarning}</p>
-  {/if}
-  <button type="submit">暗号文を生成する</button>
-</form>
+    <label>
+      パスワード:
+      {#if showPassword}
+        <input
+          type="text"
+          bind:value={password}
+          style="width: 100%;"
+          minlength="4"
+          maxlength="80"
+          class="password-input"
+        />
+        <button type="button" on:click={() => copyToClipboard(password)}
+          >パスワードをコピー</button
+        >
+      {:else}
+        <input
+          type="password"
+          bind:value={password}
+          style="width: 100%;"
+          minlength="8"
+          class="password-input"
+        />
+        <button type="button" on:click={() => copyToClipboard(password)}
+          >パスワードをコピー</button
+        >
+      {/if}
+      <button
+        type="button"
+        on:click={togglePasswordVisibility}
+        class="toggle-button"
+        >{showPassword ? "非表示にする" : "パスワードを表示する"}</button
+      >
+    </label>
+    {#if passwordWarning}
+      <p style="color: red;">{passwordWarning}</p>
+    {/if}
+    <button type="submit">暗号文を生成する</button>
+  </form>
 
   {#if cipher}
     <p>暗号文: {cipher}</p>
-    <button type="button" on:click={() => copyToClipboard(cipher)}>暗号文をコピー</button>
+    <button type="button" on:click={() => copyToClipboard(cipher)}
+      >暗号文をコピー</button
+    >
   {/if}
 </main>
 
@@ -138,7 +182,7 @@ function copyToClipboard(text: string) {
     margin-bottom: 15px;
   }
 
-   h3 {
+  h3 {
     font-size: 18px;
     margin-top: 30px;
     margin-bottom: 20px;
@@ -162,7 +206,7 @@ function copyToClipboard(text: string) {
   input {
     padding: 5px;
     font-size: 16px;
-    width: 30vw;
+    width: 40vw;
   }
 
   .toggle-button {
@@ -173,5 +217,4 @@ function copyToClipboard(text: string) {
     font-size: 16px;
     cursor: pointer;
   }
-
 </style>
--- src/lib/Greet.svelte
+++ src/lib/Greet.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
-  import { invoke } from "@tauri-apps/api/tauri"
+  import { invoke } from "@tauri-apps/api/tauri";
 
   let name = "";
-  let greetMsg = ""
+  let greetMsg = "";
 
-  async function greet(){
+  async function greet() {
     // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    greetMsg = await invoke("greet", { name })
+    greetMsg = await invoke("greet", { name });
   }
 </script>
 
